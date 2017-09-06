d0f512c95b4a6a4785212419b9c7bfa1b8d1c213
IamWaffle <ryanthecat17@gmail.com>
2017-09-06 00:23:01 -0400

    .

--- CryptoWatch/src/cryptowatch/CryptoWatch.java
+++ CryptoWatch/src/cryptowatch/CryptoWatch.java
@@ -3,7 +3,7 @@ package cryptowatch;
 public class CryptoWatch {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("ITSA MEA MARIO");
     }
     
 }
