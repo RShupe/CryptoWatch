1b9e932d8e8ffa33d87c897fe2e2f85b4c6667fc
IamWaffle <ryanthecat17@gmail.com>
2017-09-05 23:36:46 -0400

    hello world lmao

---
+++ src/main/HelloWorld.java
@@ -0,0 +1,7 @@
+package main;
+
+public class HelloWorld {
+        public static void main(String[] arge) {
+            System.out.println("I guess this is kind of how every program starts?");
+        }
+}
