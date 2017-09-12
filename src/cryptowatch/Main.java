b932e50c76fd1f9647f9bf67bfc53a3c134adcb3
pspslimboy <pspslimboy98@gmail.com>
2017-09-12 04:48:35 -0400

    Move main function into seperate class
    Also reorganized imports

---
+++ src/cryptowatch/Main.java
@@ -0,0 +1,78 @@
+package cryptowatch;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.IOException;
+import static java.lang.System.out;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.MalformedURLException;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.eclipsesource.json.*;
+import org.apache.commons.io.IOUtils;
+
+public class Main {
+    
+    public static boolean isUnix() {
+        String OS = System.getProperty("os.name").toLowerCase();
+        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+    }
+    
+    public static void main(String args[]) {
+        try {
+            if (isUnix()) {
+                System.out.println("This is Unix or Linux");
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+            } else {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            }
+        }
+        catch (UnsupportedLookAndFeelException e) {}
+        catch (ClassNotFoundException e) {}
+        catch (InstantiationException e) {}
+        catch (IllegalAccessException e) {}
+        
+        try {
+            FileWriter fw = new FileWriter("selected_currencies.txt");
+            PrintWriter pw = new PrintWriter(fw);
+            
+            pw.println("testing");
+            
+            pw.close();
+        }
+        catch (IOException e){
+            out.println("ERROR CREATING FILE");
+        }
+        
+        out.println("\nGet LTC stats test:");
+        try {
+            URL url = new URL("https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc");
+            URLConnection con = url.openConnection();
+            InputStream in = con.getInputStream();
+            String encoding = con.getContentEncoding();  // ** WRONG: should use "con.getContentType()" instead but it returns something like "text/html; charset=UTF-8" so this value must be parsed to extract the actual encoding
+            encoding = encoding == null ? "UTF-8" : encoding;
+            String body = IOUtils.toString(in, encoding);
+            //System.out.println("Raw JSON: " + body + "\n");
+            JsonArray items = Json.parse(body).asObject().get("result").asArray();
+            for (JsonValue item : items) {
+                String market = item.asObject().getString("MarketName", "Unknown Item");
+                float volume = item.asObject().getFloat("Volume", 0);
+                float last = item.asObject().getFloat("Last", 0);
+                System.out.println("Market: " + market);
+                System.out.println("Volume: " + volume + " BTC");
+                System.out.println("Last Price: " + last + " BTC");
+            }
+        }
+        catch(MalformedURLException e) {}
+        catch(IOException e) {}
+     
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainGui().setVisible(true);
+            }
+        });
+    }
+}
--- src/cryptowatch/MainGui.java
+++ src/cryptowatch/MainGui.java
@@ -1,24 +1,18 @@
 package cryptowatch;
 
-
 import java.awt.Toolkit;
-import javax.swing.DefaultListModel;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import com.eclipsesource.json.*;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import static java.lang.System.out;
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.MalformedURLException;
+import javax.swing.DefaultListModel;
 
+import com.eclipsesource.json.*;
 import org.apache.commons.io.IOUtils;
 
 public class MainGui extends javax.swing.JFrame {
-
+    
     public MainGui() {
         initComponents();
         this.setLocation(300, 100);
@@ -147,78 +141,6 @@ public class MainGui extends javax.swing.JFrame {
         //Logs Selected Currencies to a file.
         System.out.println("I'm supposed to save this list lol.");
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    
-    private static String OS = System.getProperty("os.name").toLowerCase();
-    
-    public static boolean isUnix() {
-        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
-    }
-    
-    public static void main(String args[]) {
-
-        try {
-            if (isUnix()) {
-                System.out.println("This is Unix or Linux");
-                UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
-            } else {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            }
-        }
-        catch (UnsupportedLookAndFeelException e) {
-            // handle exception
-        }
-        catch (ClassNotFoundException e) {
-            // handle exception
-        }
-        catch (InstantiationException e) {
-            // handle exception
-        }
-        catch (IllegalAccessException e) {
-            // handle exception
-        }
-        
-        try {
-            FileWriter fw = new FileWriter("selected_currencies.txt");
-            PrintWriter pw = new PrintWriter(fw);
-            
-            pw.println("testing");
-            
-            pw.close();
-        }
-        catch (IOException e){
-            out.println("ERROR CREATING FILE");
-        }
-        
-        out.println("\nGet LTC stats test:");
-        try {
-            URL url = new URL("https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc");
-            URLConnection con = url.openConnection();
-            InputStream in = con.getInputStream();
-            String encoding = con.getContentEncoding();  // ** WRONG: should use "con.getContentType()" instead but it returns something like "text/html; charset=UTF-8" so this value must be parsed to extract the actual encoding
-            encoding = encoding == null ? "UTF-8" : encoding;
-            String body = IOUtils.toString(in, encoding);
-            //System.out.println("Raw JSON: " + body + "\n");
-            JsonArray items = Json.parse(body).asObject().get("result").asArray();
-            for (JsonValue item : items) {
-                String market = item.asObject().getString("MarketName", "Unknown Item");
-                float volume = item.asObject().getFloat("Volume", 0);
-                float last = item.asObject().getFloat("Last", 0);
-                System.out.println("Market: " + market);
-                System.out.println("Volume: " + volume + " BTC");
-                System.out.println("Last Price: " + last + " BTC");
-            }
-        }
-        catch(MalformedURLException e) {}
-        catch(IOException e) {}
-     
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainGui().setVisible(true);
-            }
-        });
-        
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
