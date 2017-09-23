f1c2ae5e8ef4248ce50ae2d515f1240d2c9a015a
pspslimboy <pspslimboy98@gmail.com>
2017-09-23 00:56:32 -0400

    Add working price functionality
    My proudest commit yet

--- src/cryptowatch/Market.java
+++ src/cryptowatch/Market.java
@@ -3,19 +3,18 @@ package cryptowatch;
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonValue;
-import java.awt.List;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 import org.apache.commons.io.IOUtils;
 
 public class Market {
-    static JsonArray arrayBittrex;
+    static String rawBittrex;
     
     public static void startTimer(){
         Timer timer = new Timer();
@@ -28,9 +27,7 @@ public class Market {
                     URLConnection con = url2.openConnection();
                     InputStream in = con.getInputStream();
                     String encoding = "UTF-8";
-                    String body = IOUtils.toString(in, encoding);
-                    arrayBittrex = Json.parse(body).asObject().get("result").asArray();
-                    //System.out.println(arrayBittrex);
+                    rawBittrex = IOUtils.toString(in, encoding);
                 }
                 catch(MalformedURLException e) {}
                 catch(IOException e) {}
@@ -38,15 +35,18 @@ public class Market {
         }, 0,5000);
     }
     
-    //public static 
-    
     public static float getPrice(String exchange, String market) {
-        for (JsonValue item : arrayBittrex) {
-            float last = item.asObject().getFloat("Last", 0);
-            System.out.println(last);
-            return last;
+        if(exchange.equals("bittrex")) {
+            JsonArray arrayBittrex = Json.parse(rawBittrex).asObject().get("result").asArray();
+            int numArrays = Json.parse(rawBittrex).asObject().get("result").asArray().size();
+
+            for(int i=0; i<numArrays; i++){
+                if(market.equalsIgnoreCase(arrayBittrex.get(i).asObject().getString("MarketName", ""))) {
+                    float last = arrayBittrex.get(i).asObject().getFloat("Last", 0);
+                    return last;
+                }
+            }
         }
         return 0;
-    }
-        
+    } 
 }
